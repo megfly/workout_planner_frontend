75ca4c7c48b2856ea250f1168d3edaee433ba3a7
megfly <mflynn333@gmail.com>
2021-01-07 12:55:47 -0500

    installed react-calendar, modified css

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@
 import { connect } from 'react-redux'
 import React, { Component } from 'react'
 import './App.css';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App"> 
         <header className="App-header">
-        <h3>Workout Planner</h3>
+          <h3>Workout Planner</h3>
         </header>
+          <Calendar />
       </div>
     )
   }
@@ -22,7 +26,7 @@ const mapStateToProps = state => {
     workouts: state.workoutReducer.workouts, //found in reducer
     loading: state.workoutReducer.loading,
     exercises: state.exerciseReducer.exercises,
-    loading: state.exerciseReducer.loading
+    //loading: state.exerciseReducer.loading,
     //loading is cuz fetch is async, takes time to get promise of result, so we use loading flag
     //loading flag when we havent got our data yet
   }
