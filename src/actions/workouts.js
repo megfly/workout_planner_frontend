97e8c2fdcf8a1f7b861836046a7d862c98beff75
megfly <mflynn333@gmail.com>
2021-01-08 12:43:52 -0500

    added a post request

--- src/actions/workouts.js
+++ src/actions/workouts.js
@@ -1,7 +1,6 @@
 //action object creator function
 
-export const getWorkouts = () => {
-    
+export const getWorkouts = () => { 
     return dispatch => {
         dispatch({type: "LOADING_WORKOUTS"})
         fetch("http://localhost:3000/api/v1/workouts")
@@ -9,10 +8,32 @@ export const getWorkouts = () => {
         .then(workouts => {
             //debugger
             dispatch({type: "WORKOUTS_LOADED", payload: workouts.data}) //payload: workouts.data}
-            })
-            
+            })    
+    }
+}
+
+//passing it workout info
+export const addWorkout = (workout) => { 
+    return dispatch => {
+        dispatch({type: "ADDING_WORKOUT"})
+        fetch("http://localhost:3000/api/v1/workouts", {
+            method: "POST",
+            body: JSON.stringify(workout),
+            headers: {
+                'Content-Type': 'application/json',
+                "Accept": 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .then(workout => {
+            //debugger
+            dispatch({type: "WORKOUT_ADDED", payload: workout.data}) //payload: workouts.data}
+            })    
     }
 }
 //2 dispatches when working w thunk.. return a function.. two dispatches
 //dispatch is how you send yoru actions to the reducer
-//dispatch is form props when connected
+//dispatch is from props when connected
+
+//thunk allows us to return a function
+//redux only lets us returns actions or objects
