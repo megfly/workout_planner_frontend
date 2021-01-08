f55d2921f150bd07ab1bfafca190aaa1d7e818b6
megfly <mflynn333@gmail.com>
2021-01-08 12:33:48 -0500

    added the form to app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import WorkoutContainer from './containers/WorkoutContainer'
+import WorkoutForm from './containers/WorkoutForm';
 
 class App extends Component {
   //never arrow fct for performace
@@ -25,6 +26,8 @@ class App extends Component {
         <header className="App-header">
           <h3>Workout Planner</h3>
         </header>
+        <h2>Create Workout</h2>
+        <WorkoutForm />
           <Calendar 
             onChange={this.handleOnChange}
             value={this.state.date}
