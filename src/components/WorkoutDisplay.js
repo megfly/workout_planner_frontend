1870d55dfc5b68bdf15c20c404bee77dcc2d42d9
megfly <mflynn333@gmail.com>
2021-01-10 16:57:31 -0500

    installed react router

--- src/components/WorkoutDisplay.js
+++ src/components/WorkoutDisplay.js
@@ -1,4 +1,12 @@
 import React from 'react'
+// import {
+//     BrowserRouter as Router,
+//     Switch,
+//     Route,
+//     Link,
+//     useRouteMatch,
+//     useParams
+//   } from "react-router-dom";
 
 export default function WorkoutDisplay(props) {
     //console.log("props is workoutdisplay", props)
