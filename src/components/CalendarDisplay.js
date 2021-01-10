f6371e12b189c67718f2d97e4d47093937898b2a
megfly <mflynn333@gmail.com>
2021-01-09 19:10:32 -0500

    changed this file from a functional componenet to a class componenet. Working on displaying workouts when a certain date is clicked on the calendar

--- src/components/CalendarDisplay.js
+++ src/components/CalendarDisplay.js
@@ -1,54 +1,130 @@
-import React, { useState } from 'react';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
-//https://react-redux.js.org/api/hooks useSelector
-import { useSelector } from 'react-redux'
-//const result: any = useSelector(selector: Function, equalityFn?: Function)
+// import React, { useState } from 'react';
+// import Calendar from 'react-calendar';
+// import 'react-calendar/dist/Calendar.css';
+// //https://react-redux.js.org/api/hooks useSelector
+// import { useSelector } from 'react-redux'
+// //const result: any = useSelector(selector: Function, equalityFn?: Function)
+// import WorkoutDisplay from './WorkoutDisplay'
+
+
+// export default function CalendarDisplay(props) {
+// //console.log("calendar display props", props)
+//     //set the states of calendar date
+//     const [date, clickedDate] = useState(new Date());
+//     //console.log(date)
+//     //console.log(clickedDate)
+
+//     const workoutResults = useSelector(state => state.workoutReducer.workouts)
+//     console.log("CAL DISPLAY", workoutResults)
+
+//     //change the date when its clicked
+//     function onChange() {
+//         clickedDate(date)
+//         console.log("in the function", date)
+//         console.log("in the function", clickedDate)
+
+//         //filter the results per date and have it show only the data form those dates
+
+//         const filteredWorkouts = workoutResults.filter(workout => {
+//             //debugger
+//             //return workout.attributes 
+           
+//                 return <WorkoutDisplay 
+//                   key={workout.id}
+//                   //key={index}
+//                   title={workout.attributes.title}
+//                   duration={workout.attributes.duration}
+//                   date={workout.attributes.date}
+//                 />
+//                 // <li key={index}>{workout.attributes.title}</li>
+//             })
+        
+//         console.log("FILTERED RESULTS:", filteredWorkouts)
+//         //setResultArr(filteredResults)
+//     }
+
+//     return (
+//         <div>
+//             <Calendar
+//                 onChange={onChange}
+//                 value={date}
+//             />
+//         </div>
+//     )
+// }
+
+import React, { Component } from 'react'
+import Calendar from 'react-calendar'
+import { connect } from 'react-redux'
 import WorkoutDisplay from './WorkoutDisplay'
 
+class CalendarDisplay extends Component {
 
-export default function CalendarDisplay(props) {
-//console.log("calendar display props", props)
-    //set the states of calendar date
-    const [date, clickedDate] = useState(new Date());
-    //console.log(date)
-    //console.log(clickedDate)
-
-    const workoutResults = useSelector(state => state.workoutReducer.workouts)
-    console.log("CAL DISPLAY", workoutResults)
-
-    //change the date when its clicked
-    function onChange() {
-        clickedDate(date)
-        console.log("in the function", date)
-        console.log("in the function", clickedDate)
-
-        //filter the results per date and have it show only the data form those dates
-
-        const filteredWorkouts = workoutResults.filter(workout => {
-            //debugger
-            //return workout.attributes 
-           
-                return <WorkoutDisplay 
-                  key={workout.id}
-                  //key={index}
-                  title={workout.attributes.title}
-                  duration={workout.attributes.duration}
-                  date={workout.attributes.date}
-                />
-                // <li key={index}>{workout.attributes.title}</li>
-            })
-        
-        console.log("FILTERED RESULTS:", filteredWorkouts)
-        //setResultArr(filteredResults)
+    state = {
+        date: new Date()
     }
 
-    return (
-        <div>
-            <Calendar
-                onChange={onChange}
-                value={date}
-            />
-        </div>
-    )
+    onChangeEvent = (event) => {
+        console.log("this is the date", event)
+
+        //date clicked
+        this.setState({
+            date: event
+        })
+
+        console.log("this is state", this.state)
+         
+
+   
+        //filter data per date clicked?
+
+        console.log("this is props", this.props.workouts.attributes)
+
+    // const filteredResults = this.props.workout.map((workout, index) => {
+    //     debugger
+    //     return <WorkoutDisplay 
+    //       key={workout.id}
+    //       title={workout.attributes.title}
+    //       duration={workout.attributes.duration}
+    //       date={workout.attributes.date}
+    //     />
+    // })
+    }
+
+    render() {
+
+    // const workoutResults = this.props.workouts.map((workout, index) => {
+    //     return <WorkoutDisplay 
+    //       key={workout.id}
+    //       title={workout.attributes.title}
+    //       duration={workout.attributes.duration}
+    //       date={workout.attributes.date}
+    //     />
+    // })
+
+        return (
+            <div>
+                <Calendar
+                    onChange={this.onChangeEvent}
+                    value={this.state.date}
+                /> 
+                {/* {workoutResults} */}
+            </div>
+        )
+    }
 }
+
+//shop at teh state store and 
+//structure what our props look like
+const mapStateToProps = state => {
+    //debugger 
+  return {
+    workouts: state.workoutReducer.workouts, //found in reducer
+    loading: state.workoutReducer.loading,
+    //loading is cuz fetch is async, takes time to get promise of result, so we use loading flag
+    //loading flag when we havent got our data yet
+  }
+}
+
+export default connect(mapStateToProps)(CalendarDisplay)
+
