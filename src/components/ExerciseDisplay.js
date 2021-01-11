e8ac1e3a999b673a3167bb7c4d23178e751b28f8
megfly <mflynn333@gmail.com>
2021-01-11 14:57:48 -0500

    added a delete button and also added props to the page

--- src/components/ExerciseDisplay.js
+++ src/components/ExerciseDisplay.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import { Button } from 'react-bootstrap';
 
 export default function ExerciseDisplay(props) {
-    //debugger 
+  //debugger 
     return (
-        <div>
-            {props.name} 
-            {/* - {props.sets} - {props.reps} - {props.weight} */}
+        <div className="exercises">
+            {props.name} - sets: {props.sets} - Reps: {props.reps} - Weight: {props.weight}
+
+            <Button 
+                type="button"
+                className="btn btn-danger"
+                variant="primary"
+                id={props.id}
+
+                onClick={() => props.handleDeleteExercise(props.id)}
+
+                // onClick={() => addExercise(props.id)}
+                
+                > X
+           </Button> 
         </div>
     )
 }
