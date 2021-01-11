b50e4ab6aa81b5ba59039dfc9e3b1c14f4d98024
megfly <mflynn333@gmail.com>
2021-01-10 19:52:38 -0500

    created a presentation component to display exercies

---
+++ src/components/ExerciseDisplay.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function ExerciseDisplay(props) {
+    //debugger 
+    return (
+        <div>
+            {props.name} 
+            {/* - {props.sets} - {props.reps} - {props.weight} */}
+        </div>
+    )
+}
