f6936c87b2b043881737c0a0a4ab8707fbdeef20
megfly <mflynn333@gmail.com>
2021-01-10 19:17:18 -0500

    fixed typos and changed fetch url because the port number was changed

--- src/actions/exercises.js
+++ src/actions/exercises.js
@@ -1,8 +1,14 @@
-export const getExercises = () => {
+//action object creator function
+
+export const getExercises = () => { 
+    //debugger
     return dispatch => {
         dispatch({type: "LOADING_EXERCISES"})
-        fetch("http://localhost:3000/api/v1/exercises")
+        fetch("http://localhost:3001/api/v1/exercises")
         .then(res => res.json())
-        .then(exercises => dispatch({type: "EXERCISES_LOADED", payload: exercises}))
+        .then(exercises => {
+            //debugger
+            dispatch({type: "EXERCISES_LOADED", payload: exercises.data}) //payload: workouts.data}
+            })    
     }
 }
