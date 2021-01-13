ec9802b6512f7fc981cfc76d19147dafd2b9be46
megfly <mflynn333@gmail.com>
2021-01-12 21:16:36 -0500

    added router exact paths and imported navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // import Calendar from 'react-calendar';
 // import 'react-calendar/dist/Calendar.css';
+import NavBar from './NavBar'
 import WorkoutContainer from './containers/WorkoutContainer'
 import WorkoutForm from './containers/WorkoutForm';
 import CalendarDisplay from './components/CalendarDisplay'
@@ -9,34 +11,24 @@ import ExerciseContainer from './containers/ExerciseContainer';
 import ExerciseForm from './containers/ExerciseForm';
 
 class App extends Component {
-  //never arrow fct for performace
-  // state = {
-  //   date: new Date(),
-  //   //date: ''
-  // }
-
-  // handleOnChange = (event) => {
-  //   console.log(event)
-  //   this.setState({ 
-  //     date: event.target
-  //   });
-  // }
 
   render() {
 
     return (
+      <Router>
       <div className="App"> 
         <header className="App-header">
-          <h3>Workout Planner</h3>
+          <NavBar />
         </header>
-        <h2>Create Workout</h2>
-          <WorkoutForm />
-          <WorkoutContainer />
-          <ExerciseContainer />
-          <br /> <br /> 
-          -------------------------
-          <CalendarDisplay />
+          <switch>
+    <Route exact path="/" component={CalendarDisplay} />
+    <Route exact path="/workouts" component={WorkoutContainer} />
+    <Route exact path="/exercises" component={ExerciseContainer} />
+    <Route exact path="/workouts/new" component={WorkoutForm} />
+    <Route exact path="/workouts/:id/exercises/new" component={ExerciseForm} />
+          </switch>
       </div>
+      </Router>
     )
   }
 }
