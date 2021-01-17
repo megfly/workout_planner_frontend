12fa460df358aa16b68e459a513e69ede6aac43b
megfly <mflynn333@gmail.com>
2021-01-17 16:52:03 -0500

    created a delete exercise button, but not currently using

--- src/components/DeleteExerciseButton.js
+++ src/components/DeleteExerciseButton.js
@@ -6,22 +6,17 @@ import {
   import { Button } from 'react-bootstrap';
 
 const DeleteExerciseButton = props => {
-    console.log("delete exercise button props", props)
-  //debugger
+
     return (
-    
       <Button
-        type="button"
-        className="btn btn-danger"
-        variant="primary"
-        id={props.id}
-        workout_id={props.workout_id}
-        // onClick={() => props.handleAddExercise(props.workout_id)}
-        onClick={props.handleDeleteExercise}
-        //onClick={props.handleDeleteWorkoutExercise}
-      > X 
-      </Button>
-   
+            type="button"
+            className="btn btn-danger"
+            variant="primary"
+            id={props.id}
+            workout_id={props.workout_id}
+            onClick={props.handleDeleteExercise}
+      > X </Button>
+ 
     )
 }
 
