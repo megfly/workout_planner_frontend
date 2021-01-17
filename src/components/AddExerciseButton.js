7d3e0cc51c46cf7295b67828910c3fc75a651c00
megfly <mflynn333@gmail.com>
2021-01-17 17:09:40 -0500

    added some button styling and cleaned up file

--- src/components/AddExerciseButton.js
+++ src/components/AddExerciseButton.js
@@ -3,15 +3,17 @@ import {
     BrowserRouter as Router,
     Link
   } from "react-router-dom";
+  import { Button } from 'react-bootstrap';
 
 const AddExerciseButton = props => {
   //debugger
     return (
     <Link to={`/workouts/${props.workout_id}/exercises/new`}>
-      <button
+      <Button
+        variant="danger"
         workout_id={props.workout_id}
         // onClick={() => props.handleAddExercise(props.workout_id)}
-      > Add Exercise</button>
+      > Add Exercise</Button>
     </Link>
     )
 }
