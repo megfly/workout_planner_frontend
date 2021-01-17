2c14f0458b923a8cb0649d16b9695a62db11e191
megfly <mflynn333@gmail.com>
2021-01-17 10:37:55 -0500

    added some workout_exercise_deleted and delete_workout_exercise case statements but commented them out

--- src/reducers/workoutReducer.js
+++ src/reducers/workoutReducer.js
@@ -1,7 +1,6 @@
 export default (state = {workouts: [], loading: false}, action) => { //always must return state
     switch(action.type){
-        case("LOADING_WORKOUTS"):
-        // debugger 
+        case("LOADING_WORKOUTS"): 
             return {...state, loading: true}
         case("WORKOUTS_LOADED"):
             return {...state, 
@@ -15,13 +14,32 @@ export default (state = {workouts: [], loading: false}, action) => { //always mu
                 loading: false,
                 workouts: [...state.workouts, action.payload]
             }
-        case("DELETING_WORKOUT"):
+        case("DELETING_WORKOUT"): 
             return {...state, loading: true}
-        case("WORKOUT_DELETED"):
+        case("WORKOUT_DELETED"): 
             return {...state, 
                 loading: false,
                 workouts: state.workouts.filter(workout => workout.id != action.payload)
             }
+
+        // case("DELETING_WORKOUT_EXERCISE"):
+        // debugger 
+        //     console.log("this is workout reducer workout state", state)
+        //     //state is returning array(0).....
+        //     // need to have workouts in here......... ......................
+   
+        //         return {...state, loading: true}
+        // case("WORKOUT_EXERCISE_DELETED"):  
+        //     return {...state, 
+        //             loading: false,
+    
+        //        //state.workouts.attributes.exercises
+        //        workouts: state.workouts.filter(wo => wo.attributes.exercises.id != action.payload)
+        //     } 
+        //     //payload will be two different ids
+        //     //object key value with the workout id and key value with exercise id
+
+
         default: 
             return state
     }
@@ -29,3 +47,5 @@ export default (state = {workouts: [], loading: false}, action) => { //always mu
 
 //no 1 rule when working with state, we dont do anything destructively
     //so we use ... and not object.assign
+
+    //state.workouts.attributes.exercises.id
