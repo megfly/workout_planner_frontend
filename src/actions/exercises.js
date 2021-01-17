bea5790bdbb1d441e252ca43df1693329a4f46bb
megfly <mflynn333@gmail.com>
2021-01-17 10:25:08 -0500

    working on deleteExercise- need to figure out how to delete the exercise from the workout array and the exercise array. Also cleaned up file

--- src/actions/exercises.js
+++ src/actions/exercises.js
@@ -1,16 +1,11 @@
 //action object creator function
 
 export const getExercises = (workout_id) => { 
-    // debugger
     return dispatch => {
         dispatch({type: "LOADING_EXERCISES"})
-        // nest with the params, what we call it here- it will be called that in controller method....
-        // debugger 
         fetch(`http://localhost:3001/api/v1/workouts/${workout_id}/exercises`) 
         .then(res => res.json())
         .then(exercises => {
-            // debugger
-           //console.log(exercises.data)
             dispatch({type: "EXERCISES_LOADED", payload: exercises.data}) //payload: workouts.data}
             })    
     }
@@ -18,13 +13,8 @@ export const getExercises = (workout_id) => {
 
 //passing it workout info
 export const addExercise = (exercise, workout_id) => { 
-  // debugger
-    console.log("ADD EXERCISE", exercise)
-    console.log("WHY IS THIS THE SAME OBJECT", workout_id)
     return dispatch => {
         dispatch({type: "ADDING_EXERCISE"})
-        //fetch(`http://localhost:3001/api/v1/exercises`, {
-            debugger 
         fetch(`http://localhost:3001/api/v1/workouts/${workout_id}/exercises`, {
             method: "POST",
             body: JSON.stringify(exercise, workout_id),
@@ -35,15 +25,12 @@ export const addExercise = (exercise, workout_id) => {
         })
         .then(res => res.json())
         .then(exercise => {
-            //debugger
             dispatch({type: "EXERCISE_ADDED", payload: exercise.data}) //payload: workouts.data}
             })    
     }
 }
 
-//passing it workout info
 export const deleteExercise = (workout_id, id) => { 
- // debugger
     return dispatch => {
         dispatch({type: "DELETING_EXERCISE"})
         fetch(`http://localhost:3001/api/v1/workouts/${workout_id}/exercises/${id}`, {
@@ -56,7 +43,6 @@ export const deleteExercise = (workout_id, id) => {
         })
       
         .then(() => {
-            //debugger
             dispatch({type: "EXERCISE_DELETED", payload: workout_id, id}) //payload: workouts.data}
             })    
     }
