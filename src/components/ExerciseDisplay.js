d280f9601b1472319474477724b5806ae8f8e561
megfly <mflynn333@gmail.com>
2021-01-17 17:00:04 -0500

    cleaned up file

--- src/components/ExerciseDisplay.js
+++ src/components/ExerciseDisplay.js
@@ -1,23 +1,26 @@
 import React from 'react'
-//import { Button } from 'react-bootstrap';
 import DeleteExerciseButton from './DeleteExerciseButton';
 
 export default function ExerciseDisplay(props) {
-//debugger 
-//console.log(props)
-    return (
-        <div className="exercises">
-         
-            <h5>{props.name} </h5>
-            <p /> 
-            sets: {props.sets} - Reps: {props.reps} - Weight: {props.weight}
 
-            <DeleteExerciseButton 
+    return (
+         
+        <div className="exercises">
+            <p />
+                <h4>{props.name} </h4>
+            <p /> 
+            
+            <b> Sets:</b> {props.sets} 
+            <b> Reps:</b> {props.reps} 
+            <b> Weight:</b> {props.weight}
+
+            {/* <DeleteExerciseButton 
                 workout_id={props.workout_id}
                 id={props.id}
                 handleDeleteExercise={props.handleDeleteExercise}
-            />
-            <br />
+            /> */}
+
+                <br />
             <br />
         </div>
     )
