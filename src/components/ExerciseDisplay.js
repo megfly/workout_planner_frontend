3371819e78e4ce3ebaf7e4df575886a3285bb4f4
megfly <mflynn333@gmail.com>
2021-01-17 19:44:43 -0500

    add link_to back button '/worouts'

--- src/components/ExerciseDisplay.js
+++ src/components/ExerciseDisplay.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import DeleteExerciseButton from './DeleteExerciseButton';
+import { Button } from 'react-bootstrap';
+import {
+    BrowserRouter as Router,
+    Link
+  } from "react-router-dom";
 
 export default function ExerciseDisplay(props) {
 
@@ -7,12 +12,13 @@ export default function ExerciseDisplay(props) {
          
         <div className="exercises">
             <p />
-                <h4>{props.name} </h4>
+                <h4>{props.name} </h4> 
             <p /> 
             
-            <b> Sets:</b> {props.sets} 
-            <b> Reps:</b> {props.reps} 
-            <b> Weight:</b> {props.weight}
+            <b> Sets:</b> {props.sets} <br />
+            <b> Reps:</b> {props.reps} <br />
+            <b> Weight:</b> {props.weight} <br />
+            <Link to={`/workouts`}> <Button>Back</Button></Link>
 
             {/* <DeleteExerciseButton 
                 workout_id={props.workout_id}
@@ -20,7 +26,6 @@ export default function ExerciseDisplay(props) {
                 handleDeleteExercise={props.handleDeleteExercise}
             /> */}
 
-                <br />
             <br />
         </div>
     )
