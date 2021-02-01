ae8524aea3426e5001cb619bdfb66b96fe939b42
megfly <mflynn333@gmail.com>
2021-02-01 13:18:14 -0500

    added comments

--- src/actions/workouts.js
+++ src/actions/workouts.js
@@ -1,15 +1,22 @@
 //action object creator function
+//An action creator is a function that creates and returns an action object.
+// We typically use these so we don't have to write the action object by hand every time:
+
+//thunk needed when working with async code
 
 export const getWorkouts = () => { 
-    return dispatch => {
-        dispatch({type: "LOADING_WORKOUTS"})
-        fetch("http://localhost:3001/api/v1/workouts")
-        .then(res => res.json())
+    return dispatch => { //returns an arrow function
+        dispatch({type: "LOADING_WORKOUTS"}) //dispatches an action //(takes in an action and going to make a copy of that object and pass it off)
+        fetch("http://localhost:3001/api/v1/workouts") //fetch returns a promise that resolves into a response object
+        .then(res => res.json()) //.json() is a method of teh response object taht gets the actual data
         .then(workouts => {
             dispatch({type: "WORKOUTS_LOADED", payload: workouts.data}) //payload: workouts.data}
             })    
     }
 }
+
+//The Redux store has a method called dispatch. 
+//The only way to update the state is to call store.dispatch() and pass in an action object. 
 
 //passing it workout info
 export const addWorkout = (workout) => { 
@@ -79,3 +86,9 @@ export const deleteWorkout = (workout_id) => {
 
 //thunk allows us to return a function
 //redux only lets us returns actions or objects
+
+
+//Redux Cycle = Action Creator -> Actions -> Dispatch -> Reducers -> Store/State
+
+//Dispatch function is going to take in an action and going to make a copy of that 
+//object and passed it off to different places inside of our application
