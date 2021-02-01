0364110e01bd70521d1a3f45a12fa81994e3f0ff
megfly <mflynn333@gmail.com>
2021-02-01 15:32:43 -0500

    cleaned up file added notes

--- src/App.js
+++ src/App.js
@@ -1,38 +1,35 @@
 import React, { Component } from 'react'
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import Calendar from 'react-calendar';
-// import 'react-calendar/dist/Calendar.css';
 import NavBar from './NavBar'
 import WorkoutContainer from './containers/WorkoutContainer'
 import WorkoutForm from './containers/WorkoutForm';
 import ExerciseContainer from './containers/ExerciseContainer';
 import ExerciseForm from './containers/ExerciseForm';
 import CalendarContainer from './containers/CalendarContainer';
-//import WorkoutExercisesContainer from './containers/WorkoutExercisesContainer';
-//import WorkoutExerciseDisplay from './components/WorkoutExerciseDisplay';
 
 class App extends Component {
 
   render() {
 
     return (
+      // declaritive routing for react
       <Router>
-      <div className="App"> 
-        <header className="App-header">
-          <NavBar />
-        </header>
-          <Switch>
-    <Route exact path="/" component={CalendarContainer} />
-    <Route exact path="/workouts" component={WorkoutContainer} />
-    <Route exact path="/exercises" component={ExerciseContainer} />
-    <Route exact path="/workouts/new" component={WorkoutForm} />
-    <Route exact path="/workouts/:id/exercises/new" component={ExerciseForm} />
-    <Route exact path="/workouts/:id/exercises" component={ExerciseContainer} />
-    {/*  */}
-    {/* <Route exact path="/workouts/:id/exercises" component={ExerciseShow} /> */}
-          </Switch>
-      </div>
+        <div className="App"> 
+          <header className="App-header">
+            <NavBar />
+          </header>
+          {/* switch looks through its children routes and renders the first one whose path matches the url */}
+          {/* when it finds one, it renders that route and ignores all the others */}
+            <Switch> 
+              <Route exact path="/" component={CalendarContainer} />
+              <Route exact path="/workouts" component={WorkoutContainer} />
+              <Route exact path="/exercises" component={ExerciseContainer} />
+              <Route exact path="/workouts/new" component={WorkoutForm} />
+              <Route exact path="/workouts/:id/exercises/new" component={ExerciseForm} />
+              <Route exact path="/workouts/:id/exercises" component={ExerciseContainer} />
+            </Switch>
+        </div>
       </Router>
     )
   }
