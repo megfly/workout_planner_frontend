1e102f5de65ab2b2b96f257eda473cf3a702cead
megfly <mflynn333@gmail.com>
2021-02-01 13:18:28 -0500

    created sort functionality

--- src/containers/WorkoutContainer.js
+++ src/containers/WorkoutContainer.js
@@ -9,36 +9,100 @@ import WorkoutDisplay from '../components/WorkoutDisplay'
 class WorkoutContainer extends Component {
   //never arrow fct for performace
 
+  //assigning local state
+  state = {
+    sorted: false, 
+    // workouts: []
+  }
+
   componentDidMount(){
-    //debugger 
       this.props.getWorkouts()
   }
 
   handleDeleteWorkout = (event) => {
-  // debugger
     this.props.deleteWorkout(event.target.id)
   }
 
-  render() {
- 
-    const workoutList = this.props.workouts.map((workout, index) => {
-     // debugger 
-        return <WorkoutDisplay 
-          key={workout.id}
-          workout_id={workout.id}
-          id={workout.id}
-          title={workout.attributes.title}
-          duration={workout.attributes.duration}
-          date={workout.attributes.date}
-          handleDeleteWorkout={this.handleDeleteWorkout}
-        />
-    }) 
+ compareWorkouts = () => {
+  return function (a, b) {
 
+//the parameters (a and b)are two elements from the array
+  let nameA = a.attributes.title.toUpperCase(); // ignore upper and lowercase
+  let nameB = b.attributes.title.toUpperCase(); // ignore upper and lowercase
+ 
+
+//if a appears before the second argument (b), return negative numner
+  if (nameA < nameB) { //ascending
+    return -1;
+  }
+
+  //if first arg appears after, return positive number
+  if (nameA > nameB) { //descending
+    return 1;
+  }
+
+  // if args are equal we return 0
+  return 0;
+}}
+ 
+sortWorkouts() {
+  if (this.state.sorted === true){
+      //`Array.prototype.sort()` is destructive..... want to use spread operator to make shallow copy of workouts array and then sorting it
+    let workoutsCopy = [...this.props.workouts];
+    workoutsCopy.sort(this.compareWorkouts())
+    return workoutsCopy;
+  }else{
+    return this.props.workouts 
+  }
+}
+
+
+toggleState = () => {
+    //boolean for its either in order or not true/false
+    if (this.state.sorted) {
+      this.setState({
+        sorted: false 
+      })
+    } else {
+    //setstate for the change
+    this.setState({
+      sorted: true,
+    })
+  }
+}
+
+  render() {
+//`render()` determines what gets displayed, based on props and state
+
+// return and render are different. render is a method of react. 
+//return is just pure javascript for returning output. 
+ 
+    let workouts = this.props.workouts 
+    let workoutsUsable = this.sortWorkouts(workouts)
+ 
     return (
       <div className="Workouts">
-          {workoutList}
-      </div>
+          {/* {workoutList} */}
+          
+            {workoutsUsable.map((workout, i) => 
+              <WorkoutDisplay 
+                  key={workout.id}
+                  workout_id={workout.id}
+                  id={workout.id}
+                  title={workout.attributes.title}
+                  duration={workout.attributes.duration}
+                  date={workout.attributes.date}
+                  handleDeleteWorkout={this.handleDeleteWorkout}
+                  />
+            )}
         
+          <button
+            // onClick={this.toggleState}
+            onClick={() => this.toggleState()}
+          >
+            Sort!
+          </button>
+      </div>   
     )
    }
 }
