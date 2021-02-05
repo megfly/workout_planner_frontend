17b4db3a29c7d24a3ff808e6db3650396bb4782b
megfly <mflynn333@gmail.com>
2021-02-05 14:18:08 -0500

    removed some notes

--- src/actions/workouts.js
+++ src/actions/workouts.js
@@ -1,21 +1,19 @@
 //action object creator function
 //An action creator is a function that creates and returns an action object.
-// We typically use these so we don't have to write the action object by hand every time:
 
 //thunk needed when working with async code
 
 export const getWorkouts = () => { 
     return dispatch => { //returns an arrow function
         dispatch({type: "LOADING_WORKOUTS"}) //dispatches an action //(takes in an action and going to make a copy of that object and pass it off)
-        fetch("http://localhost:3001/api/v1/workouts") //fetch returns a promise that resolves into a response object
-        .then(res => res.json()) //.json() is a method of teh response object taht gets the actual data
+        fetch("http://localhost:3001/api/v1/workouts") 
+        .then(res => res.json()) 
         .then(workouts => {
             dispatch({type: "WORKOUTS_LOADED", payload: workouts.data}) //payload: workouts.data}
             })    
     }
 }
 
-//The Redux store has a method called dispatch. 
 //The only way to update the state is to call store.dispatch() and pass in an action object. 
 
 //passing it workout info
