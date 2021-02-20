78a70f17b3a28bc54d32385e5d01e792b77b32a3
megfly <mflynn333@gmail.com>
2021-02-19 19:14:31 -0500

    working on handleDecrementer

--- src/components/Incrementer.js
+++ src/components/Incrementer.js
@@ -1,39 +1,68 @@
 import React, { Component } from 'react'
 
  class Incrementer extends Component {
-// setup local state in order to change the count value
     state = {
-        count: 0
+        count: 0,
+        input: ''
     } 
 
     handleIncrementer = (event) => {
-        //debugger
-          //increment the counter here
-          //originally had this.state and it returned object object 
-          //because it was the whole state
-          let increment = this.state.count + 1
-         // console.log(increment)
-          
-          //console.log("this is the event", event.target.value)
-          //set the state so its the new count
+        //convert this.state.input to a number.... 
+       if (this.state.input !== '') {
+        let increment = this.state.count + parseInt(this.state.input)
+        
           this.setState({
               count: increment
           })
         }
 
+        else {
+            let increment = this.state.count + 1 
+
+            this.setState({
+                count: increment
+            })
+        }
+    }
+
         handleDecrementer = (event) => {
-
-           
-
             // We want it to decrement the count IF the count is > 0
-            if (this.state.count > 0) {
-                let decrement = this.state.count - 1
+           if (this.state.count > 0) {
+               //if input is not equal to an empty string
+                if (this.state.input !== '') {
+ 
+                    let decrement = this.state.count - parseInt(this.state.input)
+                    this.setState({
+                        count: decrement,
+                        input: ''
+                    })
+                }
+
+
+                else {
+                    let decrement = this.state.count - 1
+
+                    this.setState({
+                        count: decrement,
+                        input: ''
+                    })
+                }
+           }
+        }
+
+        handleNumberInput = (event) => {
+            //only numbers allowed in number input 
+            let re = /^[0-9\b]+$/;
+
+            // if value is not blank, then test the regex
+            if (event.target.value === '' || re.test(event.target.value)) {
                 this.setState({
-                    count: decrement
+                    input: event.target.value
                 })
             }
-            
         }
+
+
     render() {
 
         return (
@@ -42,14 +71,17 @@ import React, { Component } from 'react'
                     type="button"
                     // setup an event handler when user clicks the button
                     onClick={this.handleIncrementer}
-                   value={this.state.count}
+                    value={this.state.count}
                 >
                     Increment
                 </button>
 
-                <input>
-                </input>
-                
+                <input 
+                    value={this.state.input}
+                    placeholder="enter number"
+                    onChange={this.handleNumberInput}
+                />
+
                 Count: {this.state.count}
 
                 <button
